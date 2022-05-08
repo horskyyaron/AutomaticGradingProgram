20ce4e6607f5383cabccb5056fb7249e34821e60
Yaron Horsky <horsky.yaron@gmail.com>
2022-05-08 17:20:21 +0300

    added makefile

--- autoCheck.sh
+++ autoCheck.sh
@@ -3,21 +3,21 @@
 echo ---------------------------------------------------
 echo DIFFERENT
 echo ---------------------------------------------------
-./prog ./textComparison/different/1/1.txt ./textComparison/different/1/2.txt
-./prog ./textComparison/different/2/1.txt ./textComparison/different/2/2.txt
-./prog ./textComparison/different/3/1.txt ./textComparison/different/3/2.txt
+./comp.out ./textComparison/different/1/1.txt ./textComparison/different/1/2.txt
+./comp.out ./textComparison/different/2/1.txt ./textComparison/different/2/2.txt
+./comp.out ./textComparison/different/3/1.txt ./textComparison/different/3/2.txt
 echo ---------------------------------------------------
 
 echo ---------------------------------------------------
 echo SIMILAR 
 echo ---------------------------------------------------
-./prog ./textComparison/similar/1/1.txt ./textComparison/similar/1/2.txt
-./prog ./textComparison/similar/2/1.txt ./textComparison/similar/2/2.txt
+./comp.out ./textComparison/similar/1/1.txt ./textComparison/similar/1/2.txt
+./comp.out ./textComparison/similar/2/1.txt ./textComparison/similar/2/2.txt
 echo ---------------------------------------------------
 
 echo ---------------------------------------------------
 echo IDENTICAL
 echo ---------------------------------------------------
-./prog ./textComparison/identical/1/1.txt ./textComparison/identical/1/2.txt
-./prog ./textComparison/identical/2/1.txt ./textComparison/identical/2/2.txt
+./comp.out ./textComparison/identical/1/1.txt ./textComparison/identical/1/2.txt
+./comp.out ./textComparison/identical/2/1.txt ./textComparison/identical/2/2.txt
 echo ---------------------------------------------------
